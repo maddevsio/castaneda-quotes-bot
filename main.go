0f0a43d10f2f0298ccb42e555d0b3263e7c3ff57
puzanov <puzanov@gmail.com>
2017-01-07 15:02:51 +0600

    + gocron integrated

--- main.go
+++ main.go
@@ -3,42 +3,65 @@ package main
 import (
 	"log"
 	"gopkg.in/telegram-bot-api.v4"
+	"github.com/jasonlvhit/gocron"
 )
 
-func main() {
-	config := NewConfig("config", "yml")
-	token := config.Get("telegram-token").(string)
+var (
+	token string
+	bot *tgbotapi.BotAPI
+	config Config
+)
+
+func task() {
+	chatID := int64(7110815) // TODO: need to store chatIDs
+	messageText := "Bot initialized message"
+	msg := tgbotapi.NewMessage(chatID, messageText)
+	bot.Send(msg)
+}
+
+func initBot() {
+	config = NewConfig("config", "yml")
+	token = config.Get("telegram-token").(string)
 	log.Printf("%v\n", token)
 
-	bot, err := tgbotapi.NewBotAPI(token)
+	var err error
+	bot, err = tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+	bot.Debug = true
+}
+
+func listenAndReactInUserMessages() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
 	if err != nil {
 		log.Panic(err)
 	}
 
-	bot.Debug = true
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
 
-	chatID := int64(7110815) // TODO: need to store chatIDs
-	messageText := "Bot initialized message"
+		bot.Send(msg)
+	}
+}
 
-	msg := tgbotapi.NewMessage(chatID, messageText)
-	bot.Send(msg)
+func runCronToSendScheduledMessages() {
+	gocron.Every(3).Seconds().Do(task)
+	<- gocron.Start()
+}
 
-	//updates, err := bot.GetUpdatesChan(u)
-	//for update := range updates {
-	//	if update.Message == nil {
-	//		continue
-	//	}
-	//
-	//	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-	//
-	//	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-	//	msg.ReplyToMessageID = update.Message.MessageID
-	//
-	//	bot.Send(msg)
-	//}
+func main() {
+	initBot()
+	go listenAndReactInUserMessages()
+	runCronToSendScheduledMessages()
 }
