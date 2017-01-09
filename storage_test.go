ae588f1b15fa5e8000a5e977f55c977bd86434e4
puzanov <puzanov@gmail.com>
2017-01-09 19:11:01 +0600

    * more tests on storage

--- storage_test.go
+++ storage_test.go
@@ -9,8 +9,15 @@ func TestChatSaveAngGet(t *testing.T) {
 	c1 := Chat{1, "Blah"}
 	c1.Save()
 
-	c2 := Chat{1, ""}
+	var c2 Chat
+	c2.Id = 1
 	c2.Get()
 
 	assert.Equal(t, c2.Info, "Blah")
+
+	var c3 Chat
+	c3.Id = 2
+	c3.Get()
+
+	assert.Equal(t, c3.Info, "")
 }
