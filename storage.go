60cf1e4fc026a337accfada8fc2340f03d9fa135
puzanov <puzanov@gmail.com>
2017-01-09 19:08:39 +0600

    * storage is working, but not integrated to the bot main logic

--- storage.go
+++ storage.go
@@ -3,12 +3,11 @@ package main
 import (
 	"github.com/peterbourgon/diskv"
 	"gopkg.in/mgo.v2/bson"
-	"log"
 )
 
 type Chat struct {
-	id int64
-	info string
+	Id int64
+	Info string
 }
 
 func (c *Chat) Get() error {
@@ -18,15 +17,11 @@ func (c *Chat) Get() error {
 		CacheSizeMax: 1024 * 1024, // 1MB
 	})
 
-	bytes, err := d.Read(string(c.id))
+	bytes, err := d.Read(string(c.Id))
 	if err != nil {
 		return err
 	}
-	log.Printf("%v \n", bytes)
-	cc := Chat{}
-	bson.Unmarshal(bytes, cc)
-	log.Printf("%v \n", cc.info)
-	return nil
+	return bson.Unmarshal(bytes, c)
 }
 
 func (c *Chat) Save() error {
@@ -35,9 +30,10 @@ func (c *Chat) Save() error {
 		Transform:    func(s string) []string { return []string{} },
 		CacheSizeMax: 1024 * 1024, // 1MB
 	})
+
 	b, err := bson.Marshal(c)
 	if (err != nil) {
 		return err
 	}
-	return d.Write(string(c.id), b)
+	return d.Write(string(c.Id), b)
 }
--- storage_test.go
+++ storage_test.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"testing"
-//	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestChatSaveAngGet(t *testing.T) {
-	c1 := Chat{1, "blah"}
+	c1 := Chat{1, "Blah"}
 	c1.Save()
 
 	c2 := Chat{1, ""}
 	c2.Get()
 
-	//assert.Equal(t, c2.info, "Blah")
+	assert.Equal(t, c2.Info, "Blah")
 }
