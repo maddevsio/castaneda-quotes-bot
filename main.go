8d4bf06f2f23dc24ed1b60ac59aefa5eb46c000f
puzanov <puzanov@gmail.com>
2017-01-21 18:37:07 +0600

    * comment fix. need to work around this

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ var (
 func main() {
 	go service.ListenAndReactInUserMessages(bot, d, quotesFilePath)
 	changeGocronTimezone()
-	gocron.Every(10).Seconds().Do(func() { // this is for testing
-	//gocron.Every(1).Day().At(config.Get("send-time").(string)).Do(func() {
+	//gocron.Every(10).Seconds().Do(func() { // this is for testing
+	gocron.Every(1).Day().At(config.Get("send-time").(string)).Do(func() {
 		chats, _ := service.GetAllChats(d)
 		messageText := service.GetRandomQuote(quotesFilePath)
 		for _, chat := range chats {
