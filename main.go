570979b0731e97734c2eb83a95e738205f6b5e57
puzanov <puzanov@gmail.com>
2017-01-23 20:42:28 +0600

    * cron changed

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ var (
 func main() {
 	go service.ListenAndReactInUserMessages(bot, d, quotesFilePath)
 	changeGocronTimezone()
-	gocron.Every(10).Seconds().Do(func() {
-	//gocron.Every(1).Day().At(config.GetString("send-time")).Do(func() {
+	//gocron.Every(10).Seconds().Do(func() {
+	gocron.Every(1).Day().At(config.GetString("send-time")).Do(func() {
 		log.Printf("Scheduled start in %v", config.GetString("send-time"))
 		chats, _ := service.GetAllChats(d)
 		log.Printf("Number of chats %v" , len(chats))
