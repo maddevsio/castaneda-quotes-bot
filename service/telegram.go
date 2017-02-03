f23a9b4200db89d07cd2afc818edb495d2e1a4d2
puzanov <puzanov@gmail.com>
2017-02-03 22:13:43 +0600

    + added welcome message

--- service/telegram.go
+++ service/telegram.go
@@ -38,11 +38,33 @@ func ListenAndReactInUserMessages(bot *tgbotapi.BotAPI, d *diskv.Diskv, quotesFi
 		chat := Chat{update.Message.Chat.ID, update.Message.From.UserName}
 		log.Printf("Saving... %v", chat)
 		err = chat.Save(d)
+
 		if err != nil {
 			log.Printf("Error saving data: %v", err)
+			return
 		}
+
+		if update.Message.Text == "/start" {
+			log.Print("this is START MESSAGE")
+			SendMessage(bot, "Привет. Этот бот создан для того, чтобы сохранять вашу осознанность в постоянном тонусе. Каждый день вы будете получать одну цитату из книг Карлоса Кастанеды, главные правила и принципы жизни война. Для того, чтобы посмотреть больше цитат, кликайте на «Ещё». Да пребудет с вами Сила!", update.Message.Chat.ID)
+
+		}
+
 		SendMessageWithKeyboard(bot, GetRandomQuote(quotesFilePath), update.Message.Chat.ID)
 	}
+}
+
+func SendMessage(bot *tgbotapi.BotAPI, message string, chatID int64) {
+	msg := tgbotapi.NewMessage(chatID, message)
+
+	log.Printf("Sending this msg: %v", msg)
+	_, err := bot.Send(msg)
+	if (err != nil) {
+		log.Printf("Error sending message: %v", err)
+		return
+	}
+	log.Print("Success")
+
 }
 
 func SendMessageWithKeyboard(bot *tgbotapi.BotAPI, message string, chatID int64) {
@@ -64,3 +86,5 @@ func SendMessageWithKeyboard(bot *tgbotapi.BotAPI, message string, chatID int64)
 	}
 	log.Print("Success")
 }
+
+
